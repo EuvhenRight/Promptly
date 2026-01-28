8b5a8a5cf5bb0fa49a3c25e2fb879a70ed4cc840
r.boage <romanboage@gmail.com>
2026-01-28 17:17:46 +0100

    HederTest1

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -112,8 +112,8 @@ export default function Header() {
           </Sheet>
         </div>
 
-        <div className="flex flex-1 items-center justify-center md:justify-start">
-          <div className="relative w-full max-w-sm">
+        <div className="flex flex-1 items-center w-full md:justify-center">
+          <div className="relative w-full max-w-xl ">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
             <Input placeholder="Search prompts..." className="pl-10" />
           </div>
