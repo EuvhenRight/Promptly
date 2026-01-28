8bfe1b9abbb73197ea070c7d707eda082248d99a
Dmytro Zamai <dmytrozamai@gmail.com>
2026-01-28 20:30:26 +0000

    The page has 4 fixed vertical sections: 1) header 2) model filter bar 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import SearchBar from '@/components/home/search-bar';
 import PromptFeed from '@/components/home/prompt-feed';
+import SubHeader from '@/components/home/sub-header';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
+      <SubHeader />
       <main>
         <SearchBar />
         <div className="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
