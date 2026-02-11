352427b13c735afa5e87f14bb3984caff8750c7d
Dmytro Zamai <dmytrozamai@gmail.com>
2026-02-11 11:19:59 +0000

    так, я проаналізував трохи код і побачив проблему.
    Я в консолі бачу та

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -318,7 +318,7 @@ export default function AdminLayout({
 					</div>
 				</div>
 			</div>
-			<div className='flex flex-col'>
+			<div className='flex flex-col min-w-0'>
 				<header className='flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6'>
 					<Sheet>
 						<SheetTrigger asChild>
