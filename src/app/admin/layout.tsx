e06722f15ddae1954b58363ff3bdbaf46f98381d
Dmytro Zamai <dmytrozamai@gmail.com>
2026-02-11 11:25:50 +0000

    І ще, я хочу закріпити ліву бічну сторону, тож замість
    <div class="hid

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -217,7 +217,7 @@ export default function AdminLayout({
 					: 'md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]',
 			)}
 		>
-			<div className='hidden border-r bg-muted/40 md:block'>
+			<div className='hidden border-r bg-muted/40 md:block sticky top-0 h-screen'>
 				<div className='flex h-full max-h-screen flex-col gap-2'>
 					<div
 						className={cn(
