f0ed27ebcb33fa02c1872640b39e97fb94e634d1
Dmytro Zamai <dmytrozamai@gmail.com>
2026-02-15 18:26:11 +0000

    Чи можна цей невидимий div і логіку зробити трішки інакшою.
    "коли неви

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -195,11 +195,16 @@ export default function Home() {
 			if (loading) return
 			if (observer.current) observer.current.disconnect()
 
-			observer.current = new IntersectionObserver(entries => {
-				if (entries[0].isIntersecting && hasMore) {
-					loadMore()
-				}
-			})
+			observer.current = new IntersectionObserver(
+				entries => {
+					if (entries[0].isIntersecting && hasMore) {
+						loadMore()
+					}
+				},
+				{
+					rootMargin: '200px',
+				},
+			)
 
 			if (node) observer.current.observe(node)
 		},
