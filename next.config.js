0c325a942ad5a45102cd75626769896ec9634902
Dmytro Zamai <dmytrozamai@gmail.com>
2026-02-16 11:59:15 +0000

    Так, ти вніс багато правок. Тепер з чистого 0, не знаю про старі помилки

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'placehold.co', port: '', pathname: '/**' },
